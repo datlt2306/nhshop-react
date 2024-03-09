17b3329758ed7a427749de714e981ec3576dc8f6
datlt2306 <datlt2306@gmail.com>
2024-03-09 13:44:40 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,45 +2,47 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header>
+        <header className="header">
             <div className="container">
-                <a href="#">Logo</a>
-                <nav className="main-menu">
-                    <ul className="main-menu__list">
-                        <li className="main-menu__item">
-                            <a href="" className="main-menu__link">
-                                menu 1
-                            </a>
-                        </li>
-                        <li className="main-menu__item">
-                            <a href="" className="main-menu__link">
-                                menu 1
-                            </a>
-                        </li>
-                        <li className="main-menu__item">
-                            <a href="" className="main-menu__link">
-                                menu 1
-                            </a>
-                        </li>
-                        <li className="main-menu__item">
-                            <a href="" className="main-menu__link">
-                                menu 1
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="block">
-                    <div className="user">
-                        <span>User</span>
-                    </div>
-                    <div className="search">
-                        <span>User</span>
-                    </div>
-                    <div className="wishlist">
-                        <span>Wishlist</span>
-                    </div>
-                    <div className="cart">
-                        <span>Wishlist</span>
+                <div className="header-inner">
+                    <a href="#">Logo</a>
+                    <nav className="main-menu">
+                        <ul className="main-menu__list">
+                            <li className="main-menu__item">
+                                <a href="" className="main-menu__link">
+                                    menu 1
+                                </a>
+                            </li>
+                            <li className="main-menu__item">
+                                <a href="" className="main-menu__link">
+                                    menu 1
+                                </a>
+                            </li>
+                            <li className="main-menu__item">
+                                <a href="" className="main-menu__link">
+                                    menu 1
+                                </a>
+                            </li>
+                            <li className="main-menu__item">
+                                <a href="" className="main-menu__link">
+                                    menu 1
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="header-block">
+                        <div className="user">
+                            <span>User</span>
+                        </div>
+                        <div className="search">
+                            <span>User</span>
+                        </div>
+                        <div className="wishlist">
+                            <span>Wishlist</span>
+                        </div>
+                        <div className="cart">
+                            <span>Wishlist</span>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/News.tsx
+++ src/components/News.tsx
@@ -3,34 +3,114 @@ import React from "react";
 const News = () => {
     return (
         <section className="news">
-            <div className="section-heading">
-                <h2 className="section-heading__title">News</h2>
-            </div>
-            <div className="section-body">
-                <div className="products">
-                    <div className="product-item">
-                        <div className="product__thumbnail">
-                            <img src="https://picsum.photos/id/3/285/301" alt="" />
-                            <span className="product__salse">50%</span>
-                        </div>
-                        <div className="product-content">
-                            <h3 className="product__name">
-                                <a href="" className="product__link">
-                                    Product A
-                                </a>
-                            </h3>
-                            <a className="product__cate">Stylish cafe chair</a>
-                            <div className="product-price">
-                                <span className="product-price__new">2.500.000đ</span>
-                                <span className="product-price__old">3.500.000đ</span>
+            <div className="container">
+                <div className="section-heading">
+                    <h2 className="section-heading__title">News</h2>
+                </div>
+                <div className="section-body">
+                    <div className="products">
+                        <div className="product-item">
+                            <div className="product__thumbnail">
+                                <img src="https://picsum.photos/id/3/285/301" alt="" />
+                                <span className="product__sale">50%</span>
+                            </div>
+                            <div className="product-content">
+                                <h3 className="product__name">
+                                    <a href="" className="product__link">
+                                        Product A
+                                    </a>
+                                </h3>
+                                <a className="product__cate">Stylish cafe chair</a>
+                                <div className="product-price">
+                                    <span className="product-price__new">2.500.000đ</span>
+                                    <span className="product-price__old">3.500.000đ</span>
+                                </div>
+                            </div>
+                            <div className="product-content-extra">
+                                <button className="btn__addtocart">Add to cart</button>
+                                <div>
+                                    <span>Share</span>
+                                    <span>Compare</span>
+                                    <span>Like</span>
+                                </div>
                             </div>
                         </div>
-                        <div className="product-content-extra">
-                            <button className="btn__addtocart">Add to cart</button>
-                            <div>
-                                <span>Share</span>
-                                <span>Compare</span>
-                                <span>Like</span>
+                        <div className="product-item">
+                            <div className="product__thumbnail">
+                                <img src="https://picsum.photos/id/3/285/301" alt="" />
+                                <span className="product__sale">50%</span>
+                            </div>
+                            <div className="product-content">
+                                <h3 className="product__name">
+                                    <a href="" className="product__link">
+                                        Product A
+                                    </a>
+                                </h3>
+                                <a className="product__cate">Stylish cafe chair</a>
+                                <div className="product-price">
+                                    <span className="product-price__new">2.500.000đ</span>
+                                    <span className="product-price__old">3.500.000đ</span>
+                                </div>
+                            </div>
+                            <div className="product-content-extra">
+                                <button className="btn__addtocart">Add to cart</button>
+                                <div>
+                                    <span>Share</span>
+                                    <span>Compare</span>
+                                    <span>Like</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-item">
+                            <div className="product__thumbnail">
+                                <img src="https://picsum.photos/id/3/285/301" alt="" />
+                                <span className="product__sale">50%</span>
+                            </div>
+                            <div className="product-content">
+                                <h3 className="product__name">
+                                    <a href="" className="product__link">
+                                        Product A
+                                    </a>
+                                </h3>
+                                <a className="product__cate">Stylish cafe chair</a>
+                                <div className="product-price">
+                                    <span className="product-price__new">2.500.000đ</span>
+                                    <span className="product-price__old">3.500.000đ</span>
+                                </div>
+                            </div>
+                            <div className="product-content-extra">
+                                <button className="btn__addtocart">Add to cart</button>
+                                <div>
+                                    <span>Share</span>
+                                    <span>Compare</span>
+                                    <span>Like</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product-item">
+                            <div className="product__thumbnail">
+                                <img src="https://picsum.photos/id/3/285/301" alt="" />
+                                <span className="product__sale">50%</span>
+                            </div>
+                            <div className="product-content">
+                                <h3 className="product__name">
+                                    <a href="" className="product__link">
+                                        Product A
+                                    </a>
+                                </h3>
+                                <a className="product__cate">Stylish cafe chair</a>
+                                <div className="product-price">
+                                    <span className="product-price__new">2.500.000đ</span>
+                                    <span className="product-price__old">3.500.000đ</span>
+                                </div>
+                            </div>
+                            <div className="product-content-extra">
+                                <button className="btn__addtocart">Add to cart</button>
+                                <div>
+                                    <span>Share</span>
+                                    <span>Compare</span>
+                                    <span>Like</span>
+                                </div>
                             </div>
                         </div>
                     </div>
